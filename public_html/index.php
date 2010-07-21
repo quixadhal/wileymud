e66909a85b17093e75bd691ac7c4f4ed30a0175e
WileyMUD III <wiley@shadowlord.org>
2010-07-21 02:31:28 -0400

    Added gitweb support so we can browse ourselves on the intertubes

--- public_html/index.php
+++ public_html/index.php
@@ -33,19 +33,19 @@ WileyMUD III!<br>
 </tr>
 </table>
 <p></p>
-     As you finish your last sip of dark Nestharian Ale, the clash of steel
+<p>As you finish your last sip of dark Nestharian Ale, the clash of steel
 interrupts your dreamy repose yet again.  Running to the door, you see some
 poor besotted elf trying to cast as the villager he just tried to rob brings
-a thick wooden club down on his head.
-     <p>
-     "Get out of MY town you bastard!"  The sorry excuse that is Shylar's
+a thick wooden club down on his head.</p>
+
+<p>"Get out of MY town you bastard!"  The sorry excuse that is Shylar's
 only cityguard runs up and starts beating on the villager... blissfully
 unaware that the fool elf started the fight in the first place.  You know
 that after 12 days of playing time, you could walk out and slaughter ALMOST
 everything in the room... but it's more fun to watch the newbie get pummeled
-as Eli starts healing the villager.
-<p>
-     WileyMUD is a venerable DikuMUD, whose origins lie nearly six years in
+as Eli starts healing the villager.</p>
+
+<p>WileyMUD is a venerable DikuMUD, whose origins lie nearly six years in
 the past.  Once upon a time, a wizard named Cyric took his brand new DikuMUD
 code and began building a world to run in it.  He wisely eschewed ALL the
 boring areas that came with the game, believing that only pathetic players
@@ -53,31 +53,32 @@ would enjoy playing a game that looked just like everyone else's game.  He
 gathered such powerful names as Muthaarin'akya, Harlequin, Grimwell,
 Derkhil, Elcid, Daisy, and even Dark Muidnar.  With their help, he managed
 to terrorize the students of WMU for nearly three years with the evil
-concoction known as WileyMUD II.
-<p>
-     After a time, Cyric grew bored with his creation.  He had acquired new
+concoction known as WileyMUD II. </p>
+
+<p>After a time, Cyric grew bored with his creation.  He had acquired new
 and more devious things known as "work", and "jobs".  As the ancient sun 3/60
 gasped its last breath, so too did WileyMUD fade away.  For a time, hopes
 held that WileyMUD III would be done, but the wretched "job" saw to it that
-this never came to be.
-<p>
-     Then Dread Quixadhal, Dark Lord of VI and player of WileyMUD II,
+this never came to be.</p>
+
+<p>Then Dread Quixadhal, Dark Lord of VI and player of WileyMUD II,
 discovered a way of summoning the shades of the dead.  With a mighty blast
 of power, he travelled back in time and wrested a copy of the Source from
 Cyric's treasure horde!  A battle ensued, and some of the World was damaged
 in the scuffle.  However, he dragged Source, World, and Vile Muidnar back
 into the present, and thus began the writing of WileyMUD III.  The addition
 of Sedna and Dirk has helped to balance out our team, and occasional visits
-by Highlander and Zar go mostly unnoticed.
-<p>
-     We have a relatively quiet mud here.  We have only recently started to
+by Highlander and Zar go mostly unnoticed.</p>
+
+<p>We have a relatively quiet mud here.  We have only recently started to
 advertise our presence again, because only now have I decided that the game
 is stable and finished enough to show to any but the most dedicated.  We were
 once known as one of the hardest, yet well balanced, DikuMUD's on the planet.
 After trying several other muds recently, I have to say this is still true.
 So, if you would like to see an ALL original world that is NOT a cakewalk...
-Please stop on by!
-<p>
+Please stop on by!</p>
+
+<!--
 <table border=1 cellspacing=5 cellpadding=5 width=100%>
   <tr>
     <td>
@@ -126,8 +127,26 @@ Please stop on by!
     </td>
   </tr>
 </table>
-<a href="wiley.txt">
-<img src="gfx/clouds.gif" border=0 align=center width=177 height=115
-     alt="(docs)"> Building</a> information IS available!
+-->
+
+<table border=0 cellspacing=5 cellpadding=5 align="center" width=80%>
+  <tr>
+    <td width="30%" align="center">
+      <a href="wiley.txt">
+        <img src="gfx/clouds.gif" border=0 align="center" width=177 height=115 alt="(docs)">
+        <br />
+        Building information
+      </a> IS available!
+    </td>
+    <td width="40%"> &nbsp; </td>
+    <td width="30%" align="center">
+      <a href="gitweb.cgi?p=.git;a=summary">
+        <img src="/gitweb/git-logo.png" border=0 align="center" width=72 height=27 alt="(git)">
+        <br />
+        GIT Repository
+      </a>
+    </td>
+  </tr>
+</table>
 </body>
 </html>
