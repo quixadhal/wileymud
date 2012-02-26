33d5321083fbc2723d9fa71ec6eee8523491294f
WileyMUD III <wiley@overlord.shadowlord.org>
2012-02-26 01:54:57 -0500

    Changed reboot time to reenable daily reboots.

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 1.114w3-beta (11.12.21) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 1.115w3-beta (11.12.23) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "1.114b"
+#define VERSION_BUILD "1.115b"
 #endif /* _VERSION_H */
