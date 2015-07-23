ea17b4ac8aca7b8260ab1a992e624169165f2413
WileyMUD III <wiley@lenin.awasteoftime.net>
2015-07-23 08:44:24 -0400

    recompile of production

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 1.214w3-beta (15.07.23) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 1.215w3-beta (15.07.23) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "1.214b"
+#define VERSION_BUILD "1.215b"
 #endif /* _VERSION_H */
