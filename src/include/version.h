8cb0ebf72cb0ce48e49d6a8564b9e366d5520177
Wiley <quixadhal@gmail.com>
2018-04-07 00:18:27 +0000

    Removed postgres from init file

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 1.618w3-beta (2018-04-06) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 1.619w3-beta (2018-04-06) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "1.618b"
+#define VERSION_BUILD "1.619b"
 #define VERSION_DATE "2018-04-06"
 #endif /* _VERSION_H */
