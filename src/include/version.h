f4c7f7ccb9d9964cb31dad8279b72651d7b42642
Wiley <quixadhal@gmail.com>
2020-08-06 15:06:08 -0700

    ftime sadness

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.94w3-gamma (2020-06-13) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.95w3-gamma (2020-08-06) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.94g"
-#define VERSION_DATE "2020-06-13"
+#define VERSION_BUILD "2.95g"
+#define VERSION_DATE "2020-08-06"
 #endif /* _VERSION_H */
