6f2ba526d2e5263c39e279939016b626f169931a
Wiley Coyote <wiley@wileymud.themud.org>
2020-12-24 02:15:13 -0800

    Moved to actual hardware

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.133w3-gamma (2020-12-18) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.134w3-gamma (2020-12-23) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.133g"
-#define VERSION_DATE "2020-12-18"
+#define VERSION_BUILD "2.134g"
+#define VERSION_DATE "2020-12-23"
 #endif /* _VERSION_H */
