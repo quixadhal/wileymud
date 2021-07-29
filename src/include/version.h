40971a7ac1344cfbe7263b5df1f6c9aa1437b88e
Wiley Coyote <wiley@wileymud.themud.org>
2021-07-29 09:56:20 -0700

    Shortened message slightly

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.187w3-gamma (2021-07-28) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.188w3-gamma (2021-07-29) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.187g"
-#define VERSION_DATE "2021-07-28"
+#define VERSION_BUILD "2.188g"
+#define VERSION_DATE "2021-07-29"
 #endif /* _VERSION_H */
