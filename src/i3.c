1e2eb4e20fa216442912b65cabb4899626769592
Wiley Coyote <wiley@wileymud.themud.org>
2021-07-30 05:01:18 -0700

    typo in pinkfish codes

--- src/i3.c
+++ src/i3.c
@@ -20861,9 +20861,9 @@ void i3_daily_summary()
         }
         PQclear(res);
 
-        snprintf(output, MAX_STRING_LENGTH, "Daily Summary for ^%%^RED%%^%%^BOLD%%^[%s]%%^RESET%%^ %%^GREEN%%^%%^BOLD%%^%d messages%%^RESET%%^ from %%^YELLOW%%^%d speakers%%^RESET%%^. %s",
+        snprintf(output, MAX_STRING_LENGTH, "Daily Summary for %%^RED%%^%%^BOLD%%^[%s]%%^RESET%%^ %%^GREEN%%^%%^BOLD%%^%d messages%%^RESET%%^ from %%^YELLOW%%^%d speakers%%^RESET%%^. %s",
                 yesterday, messages, speakers, logpage_url);
-        i3_npc_speak("wiley", "Cron", output);
+        i3_npc_speak("intercre", "Cron", output);
         log_info("Summary done.");
     }
 }
