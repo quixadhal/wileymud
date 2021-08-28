40e42d6b819b72ddfbe2cf76fbfb14846de3e3b1
Wiley Coyote <wiley@wileymud.themud.org>
2021-08-28 13:16:14 -0700

    Renamed limits.c to mudlimits.c to match header

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.201w3-gamma (2021-08-27) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.202w3-gamma (2021-08-28) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.201g"
-#define VERSION_DATE "2021-08-27"
+#define VERSION_BUILD "2.202g"
+#define VERSION_DATE "2021-08-28"
 #endif /* _VERSION_H */
