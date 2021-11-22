eff17a429abebaa84885e9b42315d86814c17c12
Wiley Coyote <wiley@wileymud.themud.org>
2021-11-22 14:19:18 -0800

    typo fix

--- public_html/log/random_background_js.php
+++ public_html/log/random_background_js.php
@@ -3,20 +3,12 @@ require_once 'site_global.php';
 require_once 'random_background.php';
 require_once 'log_navigation.php';
 header("Content-Type: application/json; charset=UTF-8");
-
-//echo "var BackgroundImageList = [\n";
-//echo "\"" . implode("\",\n\"", $background_image_list) . "\"\n";
-//echo "];\n";
-
-//echo "var SpecialImageList = [\n";
-//echo "\"" . implode("\",\n\"", $special_image_list) . "\"\n";
-//echo "];\n";
 ?>
 // This depends on <script src="<?php echo $JSRANDOM;?>"></script>
 
 var BackgroundImageList = <?php echo json_encode($background_image_list); ?>;
 var SpecialImageList = <?php echo json_encode($special_image_list); ?>;
-var TodayDirExists = <?php echo $today_dir_exists ? "1" : "0"; ?>
+var TodayDirExists = <?php echo $today_dir_exists ? "1" : "0"; ?>;
 
 var Random = new MersenneTwister();
 function randomizeBackground() {
