5a90649bfa5ae7a5ef8bc1027b50c2d0436b1364
Wiley Coyote <wiley@wileymud.themud.org>
2022-03-03 18:39:25 -0800

    Got the topics from each board, mostly

--- bin/forum_snarf.pl
+++ bin/forum_snarf.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/perl -w
 
 use strict;
+use English qw( −no_match_vars );
 use HTML::TreeBuilder;
 use Data::Dumper;
 $Data::Dumper::Sortkeys = 1;
+
+#my $file_prefix = '/space/stuff/Mirrors/MudMirror-2021-10-16/lpmuds.net';
+my $file_prefix = '/space/stuff/Mirrors/MudMirror-2020-05-11/lpmuds.net';
 
 sub trim {
     my $str = shift;
@@ -15,21 +19,21 @@ sub trim {
 }
 
 sub get_boards {
-    my $file = '/space/stuff/Mirrors/MudMirror-2021-10-16/lpmuds.net/forum/index.html';
-    die "File not found." if ! -r $file;
-    my $root = HTML::TreeBuilder->new_from_file($file) or die "Can't parse file.";
-    my $table = $root->look_down(class => 'table_list') or die "No table_list found.";
-
+    my $file = "$file_prefix/forum/index.html";
+    die "File $file not found." if ! -r $file;
+    my $root = HTML::TreeBuilder->new_from_file($file) or die "Can't parse $file.";
+    my $table = $root->look_down(class => 'table_list') or die "No table_list found in $file.";
 
     my @categories = $table->look_down(id => qr/category_\d+_boards/);
+    die "No categories found in $file." if (scalar @categories) < 1;
     my $board_results = {};
     foreach my $cat (@categories) {
         my $cat_id = $cat->attr('id');
         $cat_id =~ /category_(\d+)_boards/;
         my $cat_number = $1;
-        my $parent = $cat->look_up(class => 'table_list');
-        my $cat_head = $parent->look_down(id => "category_$cat_number");
-        my $cat_info = $cat_head->look_down(class => 'catbg');
+        my $parent = $cat->look_up(class => 'table_list') or die "Cannot find table list for category $cat_id.";
+        my $cat_head = $parent->look_down(id => "category_$cat_number") or die "Cannot find category_$cat_number.";
+        my $cat_info = $cat_head->look_down(class => 'catbg') or die "Cannot get category name for $cat_id.";
         my $cat_name = trim $cat_info->as_text;
 
         $board_results->{$cat_number} = {
@@ -38,6 +42,7 @@ sub get_boards {
         };
 
         my @boards = $cat->look_down(id => qr/board_\d+/);
+        die "No boards found in category $cat_name." if (scalar @boards) < 1;
         foreach my $board (@boards) {
             my $board_id = $board->attr('id');
             $board_id =~ /board_(\d+)/;
@@ -66,7 +71,7 @@ sub get_boards {
             #  </td>
             #</tr>
 
-            my $info = $board->look_down(class => 'info');
+            my $info = $board->look_down(class => 'info') or die "Cannot get info for board $board_id.";
             #<td class="info">
             #  <a class="subject" 
             #  href="../smf/indexa950.html?PHPSESSID=atphbp79901okvnth7h9oh9jq8&amp;board=1.0" name="b1">
@@ -74,18 +79,18 @@ sub get_boards {
             #  <p>A forum for general LPC, LPmud, mostly-on-topic stuff.
             #</td>
 
-            my $anchor = $info->look_down(class => 'subject');
+            my $anchor = $info->look_down(class => 'subject') or die "Cannot get url for board $board_id.";
             my $board_url = $anchor->attr('href');
             my $board_name = trim $anchor->as_text;
 
-            my $p = $info->look_down(_tag => 'p');
+            my $p = $info->look_down(_tag => 'p') or die "Cannot get board description for $board_name.";
             my $board_desc = trim $p->as_text;
 
             #  <td class="stats windowbg">
             #    <p>1621 Posts <br /> 271 Topics
             #  </td>
-            my $stats = $board->look_down(class => 'stats windowbg');
-            my $p2 = $stats->look_down(_tag => 'p');
+            my $stats = $board->look_down(class => 'stats windowbg') or die "Cannot get stats for $board_name.";
+            my $p2 = $stats->look_down(_tag => 'p') or die "Cannot get stats for $board_name.";
             my $counts = $p2->as_text;
             $counts =~ /(\d+)\s+Posts\s+(\d+)\s+Topics/;
             my ($posts, $topics) = ($1, $2);
@@ -97,7 +102,7 @@ sub get_boards {
             #    <a href="../smf/index7910.html?PHPSESSID=atphbp79901okvnth7h9oh9jq8&amp;topic=1641.msg9147#new" title="MOVED: I3 flakiness...">MOVED: I3 flakiness...</a>
             #    <br /> on April 11, 2020, 06:24:26 pm
             #  </td>
-            my $lastpost = $board->look_down(class => 'lastpost');
+            my $lastpost = $board->look_down(class => 'lastpost') or die "Cannot get last post info for $board_name.";
             my $blah = $lastpost->as_text;
 
             # We assumne the ordering of the hrefs here!
@@ -137,40 +142,154 @@ sub get_boards {
     return $board_results;
 }
 
-my $board_results = get_boards();
-#print Dumper($board_results);
+sub get_board_topics {
+    my $board_url = shift;
+    my $current_page = shift; # pass in 1 to begin
+    $board_url =~ s!\.html\?.*$!.html!;
+    $board_url =~ s!\.\./smf/!!;
 
-foreach my $category (
-    map { $board_results->{$_} } (
-        sort { $a <=> $b } keys %$board_results
-    )
-) {
-    # insert into categories (id, name) values (?,?)
-    printf "Category            %02d\n", $category->{category_id};
-    printf "    NAME            %s\n", $category->{category_name};
-    foreach my $board (
-        map { $category->{$_} } (
-            sort { $a <=> $b } grep {!/^category_/} keys %$category
+    my $file = "$file_prefix/smf/$board_url";
+    die "File $file not found." if ! -r $file;
+    my $root = HTML::TreeBuilder->new_from_file($file) or die "Can't parse $file.";
+    my $pagelinks = $root->look_down(class => 'pagelinks') or die "No page list found in $file.";
+    my @page_links = $pagelinks->look_down(class => 'navPages');
+    my $next_page_url = undef;
+    my $next_page = undef;
+
+    if((scalar @page_links) < 1) {
+        #warn "No navigation links in $board_url";
+        #return undef;
+    } else {
+        foreach my $link (@page_links) {
+            my $page_number = $link->as_text;
+            next if $page_number <= $current_page;
+            $next_page = $page_number;
+            $next_page_url = $link->attr('href');
+            last;
+        }
+    }
+
+    # Here, we'd process the topic list on THIS page, and then...
+    #
+    my $topic_table = $root->look_down(id => 'messageIndex') or die "No topic list found on page $current_page.";
+    my $topic_grid = $topic_table->look_down(class => 'table_grid') or die "No topic list found on page $current_page.";
+    my $table = $topic_grid->look_down(_tag => 'tbody') or die "No topic list found on page $current_page.";
+    my @rows = $table->look_down(_tag => 'tr') or die "No topics found on page $current_page.";
+    die "No topic rows found on page $current_page." if (scalar @rows) < 1;
+    my @topic_data = ();
+    foreach my $row (@rows) {
+        my @cols = $row->look_down(_tag => 'td') or die "No topics columns found on page $current_page.";
+        die "Expected 5 columns, only got ".(scalar @cols) if (scalar @cols) < 5;
+
+        my $topic_span = $cols[2]->look_down(_tag => 'span') or die "Cannot find message id.";
+        my $topic_message_id = $topic_span->attr('id'); # msg_NNN
+        $topic_message_id =~ s/msg_//;
+
+        my $topic_started_by_blob = $cols[2]->look_down(_tag => 'p') or die "No topic started by blob.";
+        my $topic_started_by_a = $topic_started_by_blob->look_down(_tag => 'a');
+        my $topic_started_by_url = "";
+        my $topic_started_by_name = "";
+        if(defined $topic_started_by_a) {
+            $topic_started_by_url = $topic_started_by_a->attr('href');
+            $topic_started_by_name = trim $topic_started_by_a->as_text;
+        } else {
+            #printf "TOPIC in $file STARTED BY BLOB: %s\n", $topic_started_by_blob->as_text;
+            # Started by daelaskai
+            $topic_started_by_blob->as_text =~ /^Started\s+by\s+(.*)\s*$/;
+            $topic_started_by_name = $1;
+        }
+
+        my $topic_a = $topic_span->look_down(_tag => 'a') or die "No topic URL.";
+        my $topic_desc = $topic_a->as_text;
+        my $topic_url = $topic_a->attr('href');
+        $topic_url =~ /\.html\?topic=(\d+)\.\d+/;
+        my $topic_id = $1;
+
+        my $stats_blob = $cols[3]->as_text or warn "Stats not available.";
+        $stats_blob =~ /(\d+)\s+Replies\s+(\d+)\s+Views/;
+        my ($topic_replies, $topic_views) = ($1,$2);
+
+        my $lastpost = $cols[4];
+        my $last_blob = trim $lastpost->as_text;
+        my @last_urls = $lastpost->look_down(_tag => 'a'); # should be 2
+        my $last_post_url = $last_urls[0]->attr('href');
+        my $last_post_profile_url = "";
+        my $last_post_profile_name = "";
+        my $last_post_date = "";
+        if((scalar @last_urls) < 2) {
+            #printf "LAST BLOB in $file: %s\n", $lastpost->as_text;
+            # December 16, 2009, 01:49:20 pm by Kalinash
+            $last_blob =~ /^(.*?\s+[ap]m)\s+by\s+(.*)\s*$/;
+            ($last_post_profile_name, $last_post_date) = ($1,$2);
+        } else {
+            $last_post_profile_url = $last_urls[1]->attr('href');
+            $last_post_profile_name = trim $last_urls[1]->as_text;
+            $last_blob =~ /^(.*?\s+[ap]m)\s+by/;
+            $last_post_date = $1;
+        }
+
+        push @topic_data, {
+            topic_id                => $topic_id,
+            topic_message_id        => $topic_message_id,
+            topic_desc              => $topic_desc,
+            topic_url               => $topic_url,
+            topic_started_by_name   => $topic_started_by_name,
+            topic_started_by_url    => $topic_started_by_url,
+            topic_replies           => $topic_replies,
+            topic_views             => $topic_views,
+            last_post_url           => $last_post_url,
+            last_post_date          => $last_post_date,
+            last_post_profile_name  => $last_post_profile_name,
+            last_post_profile_url   => $last_post_profile_url,
+
+        };
+    }
+
+    return {
+        current_page            => $current_page,
+        current_page_url        => $board_url,
+        next_page               => $next_page,
+        next_page_url           => $next_page_url,
+        topics                  => \@topic_data,
+    };
+}
+
+sub show_board_data {
+    my $board_results = shift;
+
+    foreach my $category (
+        map { $board_results->{$_} } (
+            sort { $a <=> $b } keys %$board_results
         )
     ) {
-        # insert into categories_boards (category_id, board_id) values (?,?)
-        # insert into boards (id, name, url, desc) values (?,?,?,?)
-        printf "    Board           %02d\n", $board->{board_id};
-        printf "        NAME        %s\n", $board->{board_name};
-        printf "        DESC        %s\n", $board->{board_desc};
-        printf "        TOPICS      %d\n", $board->{topics};
-        printf "        POSTS       %d\n", $board->{posts};
-        printf "        LAST POST   %s\n", $board->{last_post}{post_date};
-        printf "            Author  %s (user %d)\n",
-            $board->{last_post}{profile_name},
-            $board->{last_post}{profile_id};
-        printf "            Post    %s (topic %d, message %d)\n",
-            $board->{last_post}{post_title},
-            $board->{last_post}{post_topic_id},
-            $board->{last_post}{post_message_id};
+        # insert into categories (id, name) values (?,?)
+        printf "Category            %02d\n", $category->{category_id};
+        printf "    NAME            %s\n", $category->{category_name};
+        foreach my $board (
+            map { $category->{$_} } (
+                sort { $a <=> $b } grep {!/^category_/} keys %$category
+            )
+        ) {
+            # insert into categories_boards (category_id, board_id) values (?,?)
+            # insert into boards (id, name, url, desc) values (?,?,?,?)
+            printf "    Board           %02d\n", $board->{board_id};
+            printf "        NAME        %s\n", $board->{board_name};
+            printf "        DESC        %s\n", $board->{board_desc};
+            printf "        TOPICS      %d\n", $board->{topics};
+            printf "        POSTS       %d\n", $board->{posts};
+            printf "        LAST POST   %s\n", $board->{last_post}{post_date};
+            printf "            Author  %s (user %d)\n",
+                $board->{last_post}{profile_name},
+                $board->{last_post}{profile_id};
+            printf "            Post    %s (topic %d, message %d)\n",
+                $board->{last_post}{post_title},
+                $board->{last_post}{post_topic_id},
+                $board->{last_post}{post_message_id};
+        }
     }
 }
 
+my $board_results = get_boards();
 
 # For each board, the url to show topics looks like
 # lpmuds.net/smf/indexd0e9.html?board=1.0
@@ -197,102 +316,41 @@ foreach my $category (
 # The other option is to search the URL part and do (X-1*20).
 #
 
+my $topic_results = {};
+
 foreach my $category (
     map { $board_results->{$_} } (
         sort { $a <=> $b } keys %$board_results
     )
 ) {
+    $topic_results->{$category->{category_id}} = {};
+
     foreach my $board (
         map { $category->{$_} } (
             sort { $a <=> $b } grep {!/^category_/} keys %$category
         )
     ) {
-        my $board_id = $board->{board_id};
-        my $board_url = $board->{board_url};
-        $board_url =~ s!\.html\?.*$!.html!;
+        my @topics = ();
+        my $url = $board->{board_url};
+        my $page = 1;
 
-        my $current_page = 1;
-        my $file = "/space/stuff/Mirrors/MudMirror-2021-10-16/lpmuds.net/forum/$board_url";
-        print "FILE: $file\n";
-        die "File not found." if ! -r $file;
-        my $root = HTML::TreeBuilder->new_from_file($file) or die "Can't parse file.";
-        my $pagelinks = $root->look_down(class => 'pagelinks') or die "No page list found.";
-        my @page_links = $pagelinks->look_down(class => 'navPages');
-        my $next_page_url = undef;
-        my $next_page = undef;
-
-        foreach my $link (@page_links) {
-            my $page_number = $link->as_text;
-            next if $page_number <= $current_page;
-            $next_page = $page_number;
-            $next_page_url = $link->attr('href');
-            last;
+        while( my $topic_chunk = get_board_topics($url, $page) ) {
+            $page = $topic_chunk->{next_page};
+            $url = $topic_chunk->{next_page_url};
+            push @topics, ( @{ $topic_chunk->{topics} } );
+            last if !defined $url;
         }
-
-        print "Current Page:    $current_page\n";
-        print "Next Page:       $next_page\n"       if defined $next_page;
-        print "URL:             $next_page_url\n"   if defined $next_page_url;
-
-        # Here, we'd process the topic list on THIS page, and then...
-        #
-        my $topic_table = $root->look_down(id => 'messageIndex') or die "No topic list found.";
-        my $topic_grid = $topic_table->look_down(class => 'table_grid') or die "No topic list found.";
-        my $table = $topic_grid->look_down(_tag => 'tbody') or die "No topic list found.";
-        my @rows = $table->look_down(_tag => 'tr') or die "No topics found.";
-        my @topic_data = ();
-        foreach my $row (@rows) {
-            my @cols = $row->look_down(_tag => 'td') or die "No topics columns found.";
-
-            my $topic_span = $cols[2]->look_down(_tag => 'span');
-            my $topic_message_id = $topic_span->attr('id'); # msg_NNN
-            $topic_message_id =~ s/msg_//;
-
-            my $topic_started_blob = $cols[2]->look_down(_tag => 'p');
-            my $topic_started_a = $topic_started_blob->look_down(_tag => 'a');
-            my $topic_started_by_url = $topic_started_a->attr('href');
-            my $topic_started_by_name = $topic_started_a->as_text;
-
-            my $topic_a = $topic_span->look_down(_tag => 'a');
-            my $topic_desc = $topic_a->as_text;
-            my $topic_url = $topic_a->attr('href');
-            $topic_url =~ /\.html\?topic=(\d+)\.\d+/;
-            my $topic_id = $1;
-
-            my $stats_blob = $cols[3]->as_text;
-            $stats_blob =~ /(\d+)\s+Replies\s+(\d+)\s+Views/;
-            my ($topic_replies, $topic_views) = ($1,$2);
-
-            my $lastpost = $cols[4];
-            my $last_blob = trim $lastpost->as_text;
-            my @last_urls = $lastpost->look_down(_tag => 'a'); # should be 2
-            my $last_post_url = $last_urls[0]->attr('href');
-            my $last_post_profile_url = $last_urls[1]->attr('href');
-            my $last_post_profile_name = $last_urls[1]->as_text;
-
-            $last_blob =~ /^(.*?\s+[ap]m)\s+by/;
-            my $last_post_date  = $1;
-
-            push @topic_data, {
-                topic_id                => $topic_id,
-                topic_message_id        => $topic_message_id,
-                topic_desc              => $topic_desc,
-                topic_url               => $topic_url,
-                topic_started_by_name   => $topic_started_by_name,
-                topic_started_by_url    => $topic_started_by_url,
-                topic_replies           => $topic_replies,
-                topic_views             => $topic_views,
-                last_post_url           => $last_post_url,
-                last_post_date          => $last_post_date,
-                last_post_profile_name  => $last_post_profile_name,
-                last_post_profile_url   => $last_post_profile_url,
-            };
-        }
-
-        print Dumper(\@topic_data);
-
-        # At this point, we'd walk down that page to get the next page, etc, etc.
-        exit 1;
-
+        printf "Category %d (%s), Board %d (%s), Topics: %d\n",
+            $category->{category_id}, $category->{category_name},
+            $board->{board_id}, $board->{board_name},
+            (scalar @topics);
+        $topic_results->{$category->{category_id}}{$board->{board_id}} = \@topics;
     }
 }
 
+print Dumper($topic_results);
+
+# Interesting.. /space/stuff/Mirrors/MudMirror-2020-05-11/lpmuds.net/smf/index0e50.html
+# Looks like the format changed?  We may have to adapt, as the forum might have started
+# using a newer format for the last entries...
+#
