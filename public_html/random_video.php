93213d3cf18e75f27ec12283d8ad84b21c88a47e
Wiley Coyote <wiley@wileymud.themud.org>
2023-03-10 05:20:57 -0800

    More junk removed

--- public_html/random_video.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $playlist_list = file("/home/wiley/public_html/autoplaylist.txt", FILE_SKIP_EMPTY_LINES);
-    $url = sprintf("Location: %s", $playlist_list[array_rand($playlist_list)]);
-    header($url);
-    die("You are no fun.");
-?>
