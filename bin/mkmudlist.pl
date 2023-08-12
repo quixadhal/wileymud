7da8badea101075aa7cb487d527c5d5c16f18be6
Wiley Coyote <wiley@wileymud.themud.org>
2023-08-12 07:01:25 -0700

    Added support to grab MSSP crawler output too

--- bin/mkmudlist.pl
+++ bin/mkmudlist.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/perl -w
+#/usr/bin/perl -w -CSAD
 
 use strict;
 use utf8;
+#binmode(STDOUT, ":utf8");
+#binmode(STDERR, ":utf8");
 use English qw( −no_match_vars );
 use Data::Dumper;
 
@@ -37,14 +40,18 @@ $HTML::FromANSI::Options{lf_to_crlf} = 1;
 $HTML::FromANSI::Options{show_cursor} = 0;
 $HTML::FromANSI::Options{style} = "line-height: 1.0; letter-spacing: 0; font-size: 12pt";
 
+my $mssp_url = 'https://mudhalla.net/tintin/protocols/mssp/mudlist.html';
+my $mssp_page = "/home/www/log/data/mssp_mudlist.html";
 my $filename = "/home/www/log/data/mudlist.json";
+my $mssp_filename = "/home/www/log/data/mssp_mudlist.json";
 my $imagedir = "/home/www/log/gfx/mud";
 my $thread_count = 60;
 my @global_tmp = ();
 my @result_set = ();
 our $default_ttype = "ansi";
 
-do_build();
+#do_build();
+do_mssp();
 exit 0;
 
 sub ansi2png {
@@ -65,7 +72,7 @@ sub subopt_callback {
         $telcmd = pack("C4 A* C2", TELNET_IAC, TELNET_SB, TELOPT_TTYPE, 0,
                        $default_ttype, TELNET_IAC, TELNET_SE);
         $biteme->put(String => $telcmd, Telnetmode => 0);
-        #printf STDERR "                Replying to TTYPE with %s\n", $default_ttype;
+        printf STDERR "                Replying to TTYPE with %s\n", $default_ttype;
     }
 
     1;
@@ -193,6 +200,7 @@ sub geoip_locate {
     $output->{'ipaddress'} = $result->{'ipaddress'};
     $output->{'name'} = $result->{'name'};
     $output->{'geoip'} = $data;
+    $output->{'login'} = $result->{'login'};
     # curl 'https://freegeoip.app/json/104.156.100.167'
     print FP encode_json($output);
     close FP;
@@ -208,9 +216,20 @@ sub get_mudlist {
         <$fp>;
     };
     my $json = decode_json(encode_utf8($page));
-    if(defined $page) {
+    print STDERR "done.";
+    print STDERR "\nParsing $mssp_filename ...";
+    my $mssp_page = do {
+        local $/ = undef; # No record seperator
+        open my $fp, "<", $mssp_filename or warn "Cannot open $mssp_filename: $!";
+        <$fp>;
+    };
+    my $mssp_json = decode_json(encode_utf8($mssp_page)) if defined $mssp_page;
+
+    if(defined $page or defined $mssp_page) {
         print STDERR "done, processing ...";
-        my @mudlist = @{ $json->{mudlist} };
+        my @mudlist = ();
+        push @mudlist, @{ $json->{mudlist} } if defined $page;
+        push @mudlist, @{ $mssp_json->{mudlist} } if defined $mssp_page;
         # Each entry should be key/value for name, type, mudlib, ipaddress, port, online.
         #print STDERR "MUDLIST: " . Dumper(\@mudlist) . "\n";
         #splice @mudlist, 10;
@@ -258,5 +277,154 @@ sub do_build {
     get_mudlist($pm);
     #print "\nFINAL:\n" . Dumper(\@result_set);
     print "\nDONE!\n";
+}
+
+sub get_url {
+    my $url = shift;
+
+    return undef if !defined $url;
+    my $lwp = LWP::UserAgent->new();
+       $lwp->timeout(30);
+    my $request = HTTP::Request->new(GET => $url);
+    my $response = undef;
+    #print "Fetching... $url\n";
+    if($request) {
+        eval {
+            local $SIG{ALRM} = sub { die "Exceeded Timeout of 30 seconds for $url\n" };
+            alarm 30;
+            $response = $lwp->request($request);
+            alarm 0;
+        };
+        warn "Timeout" if($EVAL_ERROR and ($EVAL_ERROR =~ /^Exceeded Timeout/));
+        return $response->content if((defined $response) and $response->is_success);
+    }
+    return undef;
+}
+
+sub do_mssp {
+    print STDERR "\nFetching $mssp_url ...";
+    #my $page = get_url($mssp_url);
+
+    system "/usr/bin/wget -O '$mssp_page' '$mssp_url'";
+    my $page = do {
+        local $/ = undef; # No record seperator
+        open my $fp, "<:encoding(UTF-8)", $mssp_page or die "Cannot open $mssp_page: $!";
+        <$fp>;
+    };
+
+    # The fields show up in this order (currently)... with lots of
+    # other fields we don't care about in between them.
+    #
+    # </span><span style='color:#FF5'>┌──
+    #
+    # <span styleblahblah>     PORT</span>              -- port
+    # <span styelblahblah>     1234</span>
+    #
+    # <span styleblahblah>     CODEBASE</span>          -- mudlib
+    # <span styelblahblah>     text</span>
+    #
+    # <span styleblahblah>     IP</span>                -- ipaddress
+    # <span styelblahblah>     123.123.123.123</span>
+    #
+    # <span styleblahblah>     NAME</span>              -- name
+    # <span styelblahblah>     Foo MUD</span>
+    #
+    # <span styleblahblah>     FAMILY</span>            -- type
+    # <span styelblahblah>     text</span>
+    #
+    # ├────
+    #
+    # ──────┘
+    # </span>
+    #
+
+    printf "Page is %d characters long\n", (length $page);
+    my @entries = (
+        #$page =~ /(<span\s+style='.*?'>\s*?[\u250F][\u2500\u2501].*?[\u2500\u2501][\u2508\u2509]\s*<\/span>)/gs
+        #$page =~ /([\u250C\u250D\u250E\u250F][\u2500\u2501]+.*?[\u2500\u2501]+[\u2518\u2519\u251A\u251B])/gs
+        #$page =~ /(┌.*?┘)/gs
+        #$page =~ /<span style='color:#FF5'>┌─.*?─┘/gs
+        #$page =~ /([\u250c].*?[\u2518])/gs
+        $page =~ /([\x{250C}].*?[\x{2518}])/gs
+    );
+    printf "Got %d entries\n", (scalar @entries);
+    my @mssp_list = ();
+    foreach my $entry (@entries) {
+        #printf "--------\n";
+        #printf "%s\n", $entry;
+        #printf "--------\n";
+
+        $entry =~ /<span.*?>\s*?NAME\s*?<\/span>\s*<span.*?>\s*(.*?)\s*<\/span>/s;
+        my $name = ($1);
+        $entry =~ /<span.*?>\s*?IP\s*?<\/span>\s*<span.*?>\s*(.*?)\s*<\/span>/s;
+        my $ipaddress = ($1);
+        $entry =~ /<span.*?>\s*?PORT\s*?<\/span>\s*<span.*?>\s*(.*?)\s*<\/span>/s;
+        my $port = ($1);
+        $entry =~ /<span.*?>\s*?FAMILY\s*?<\/span>\s*<span.*?>\s*(.*?)\s*<\/span>/s;
+        my $type = ($1);
+        $entry =~ /<span.*?>\s*?CODEBASE\s*?<\/span>\s*<span.*?>\s*(.*?)\s*<\/span>/s;
+        my $mudlib = ($1);
+
+        next if !defined $name or -z $name;
+        next if !defined $ipaddress or -z $ipaddress;
+        next if !defined $port or -z $port;
+
+        my $data = {
+            name        => $name,
+            md5         => md5_hex($name),
+            type        => $type,
+            mudlib      => $mudlib,
+            ipaddress   => $ipaddress,
+            port        => $port,
+            online      => 0,
+            from_mssp   => 1,
+        };
+        push @mssp_list, $data;
+        printf "%s  %s:%s  type: %s mudlib: %s\n", $name, $ipaddress, $port, $type, $mudlib;
+    }
+
+    #my $dump = { mudlist => \@mssp_list };
+    #my $json_dump = JSON->new->utf8->allow_nonref->canonical->pretty->encode({ mudlist => \@mssp_list });
+    #printf "--------\n";
+    #printf "%s", $json_dump;
+    #printf "--------\n";
+
+    print STDERR "\nParsing $filename ...";
+    my $mudlist_page = do {
+        local $/ = undef; # No record seperator
+        open my $fp, "<", $filename or die "Cannot open $filename: $!";
+        <$fp>;
+    };
+    print STDERR "done.\n";
+    my $mudlist_json = decode_json(encode_utf8($mudlist_page));
+    my @mudlist = @{ $mudlist_json->{mudlist} };
+    printf "Have %d mudlist entries in JSON file\n", (scalar @mudlist);
+
+    my @new_mssp = ();
+    foreach my $mssp (@mssp_list) {
+        my @name_dup = grep  {lc $_->{name} eq lc $mssp->{name}} (@mudlist);
+        my @ip_dup = grep { lc $_->{ipaddress} eq lc $mssp->{ipaddress} and lc $_->{port} eq lc $mssp->{port}} (@mudlist);
+        if ((scalar @name_dup) > 0) {
+            printf "Duplicate by name   %s %s:%s %s\n", $mssp->{name}, $mssp->{ipaddress}, $mssp->{port}, $mssp->{md5};
+            next;
+        }
+        if ((scalar @ip_dup) > 0) {
+            printf "Duplicate by ip:    %s %s:%s %s\n", $mssp->{name}, $mssp->{ipaddress}, $mssp->{port}, $mssp->{md5};
+            next;
+        }
+        push @new_mssp, $mssp;
+    }
+
+    printf "Found %d new muds to process!\n", (scalar @new_mssp);
+    my $dump = JSON->new->utf8->allow_nonref->canonical->pretty->encode({ mudlist => \@new_mssp });
+    open FP, ">:encoding(UTF-8)", "$mssp_filename" or die "Cannot open MSSP JSON file for output: $!";
+    print FP "$dump\n";
+    close FP;
+
+    # At this point, we need to do what do_build() and get_mudlist() do to process
+    # all the muds NOT already done by those... or merge this into that so we
+    # can do both at once from both files...
+
+    do_build();
 }
 
