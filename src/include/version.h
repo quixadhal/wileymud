96e2a85f3f7b1ce7735579173b49ddef1a1c7724
Wiley Coyote <wiley@wileymud.themud.org>
2023-10-27 10:09:30 -0700

    Finished moving to VM

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.301w3-gamma (2023-08-14) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.302w3-gamma (2023-10-27) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.301g"
-#define VERSION_DATE "2023-08-14"
+#define VERSION_BUILD "2.302g"
+#define VERSION_DATE "2023-10-27"
 #endif /* _VERSION_H */
