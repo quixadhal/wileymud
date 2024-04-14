79898926123467562a671d1e3b908e146c7e3945
Wiley Coyote <wiley@wileymud.themud.org>
2024-04-13 20:12:40 -0700

    enable discord

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.306w3-gamma (2024-03-28) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.307w3-gamma (2024-04-13) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.306g"
-#define VERSION_DATE "2024-03-28"
+#define VERSION_BUILD "2.307g"
+#define VERSION_DATE "2024-04-13"
 #endif /* _VERSION_H */
