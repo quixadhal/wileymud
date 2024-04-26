161a9b77124643b1198d14f03d4bb6b3fe651fb2
Wiley Coyote <wiley@wileymud.themud.org>
2024-04-26 16:49:44 -0700

    Sali owns it now

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Quixadhal's Version 2.308w3-gamma (2024-04-13) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Salius's Version 2.309w3-gamma (2024-04-26) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.308g"
-#define VERSION_DATE "2024-04-13"
+#define VERSION_BUILD "2.309g"
+#define VERSION_DATE "2024-04-26"
 #endif /* _VERSION_H */
