d280bfc0eb2573819f86829905faa589cc5b90f9
Wiley Coyote <wiley@wileymud.themud.org>
2024-04-26 17:04:27 -0700

    Messages hardcoded in i3 for color tags

--- src/i3.c
+++ src/i3.c
@@ -17785,7 +17785,7 @@ void i3_log_alive()
 
     snprintf(taunt, MAX_STRING_LENGTH,
              "%%^RED%%^%%^BOLD%%^[%-4.4d-%-2.2d-%-2.2d %-2.2d:%-2.2d]%%^RESET%%^ %%^GREEN%%^%%^BOLD%%^%s "
-             "(%s)%%^RESET%%^ %%^YELLOW%%^*** Welcome to WileyMUD III, Quixadhal's Version %s (%s) ***%%^RESET%%^",
+             "(%s)%%^RESET%%^ %%^YELLOW%%^*** Welcome to WileyMUD III, Salius's Version %s (%s) ***%%^RESET%%^",
              tm_info->tm_year + 1900, tm_info->tm_mon + 1, tm_info->tm_mday, tm_info->tm_hour, tm_info->tm_min,
              "It's ALIVE!\r\n", I3_ROUTER_NAME, VERSION_BUILD, VERSION_DATE);
     i3_npc_speak("wiley", "Cron", taunt);
@@ -17802,7 +17802,7 @@ void i3_log_dead()
 
     snprintf(taunt, MAX_STRING_LENGTH,
              "%%^RED%%^%%^BOLD%%^[%-4.4d-%-2.2d-%-2.2d %-2.2d:%-2.2d]%%^RESET%%^ %%^RED%%^%%^BOLD%%^%s (%s)%%^RESET%%^ "
-             "%%^YELLOW%%^*** Welcome to WileyMUD III, Quixadhal's Version %s (%s) ***%%^RESET%%^",
+             "%%^YELLOW%%^*** Welcome to WileyMUD III, Salius's Version %s (%s) ***%%^RESET%%^",
              tm_info->tm_year + 1900, tm_info->tm_mon + 1, tm_info->tm_mday, tm_info->tm_hour, tm_info->tm_min,
              "It's going DOWN!\r\n", I3_ROUTER_NAME, VERSION_BUILD, VERSION_DATE);
     allchan_log(0, (char *)"wiley", (char *)"Cron", (char *)"Cron", (char *)"WileyMUD", taunt);
