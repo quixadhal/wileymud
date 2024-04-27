04c5df181937d692a3e02698e4d69b59b7b9e132
Wiley Coyote <wiley@wileymud.themud.org>
2024-04-26 17:04:09 -0700

    Forgot the hardcoded i3 messages, rebuilt

--- src/include/version.h
+++ src/include/version.h
@@ -3,8 +3,8 @@
 #define _VERSION_H
 
 #define VERSION_STR \
-"\r\n*** Welcome to WileyMUD III, Salius's Version 2.309w3-gamma (2024-04-26) ***\r\n"
+"\r\n*** Welcome to WileyMUD III, Salius's Version 2.310w3-gamma (2024-04-26) ***\r\n"
 #define VERSION_BASE "WileyMUD III"
-#define VERSION_BUILD "2.309g"
+#define VERSION_BUILD "2.310g"
 #define VERSION_DATE "2024-04-26"
 #endif /* _VERSION_H */
